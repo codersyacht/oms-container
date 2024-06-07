dc8bc3ffb6f29618dbb87bf77d5970ced6060915
codersyacht <codersyacht@gmail.com>
2024-06-06 22:15:59 -0700

    v1

--- portforward.sh
+++ portforward.sh
@@ -1,1 +1,1 @@
-kubectl port-forward --address 0.0.0.0 svc/ingress-nginx-controller -n ingress-nginx 443:443
+oc port-forward --address 0.0.0.0 svc/router-internal-default -n openshift-ingress 443:443
